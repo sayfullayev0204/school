8e05264093959202c694805e1269531a317391c7
G'ayrat Sayfullayev <136235060+sayfullayev0204@users.noreply.github.com>
2024-05-21 21:23:12 +0500

    Update urls.py

--- task/urls.py
+++ task/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from .views import home,tek,fan,uzifa,sinf,info
+from .views import SelectClass
 urlpatterns = [
     path('', home, name='home'),
-    path('sinf/', sinf, name='sinf'),
+    path('sinf/', SelectClass.as_view(), name='sinf'),
     path('tek/', tek, name='tek'),
     path('fan/', fan, name='fan'),
     path('vazifa/', uzifa, name='vazifa'),
